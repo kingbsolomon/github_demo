b0e8a085e39d4fce96ff38c047bd8c7686704db5
kingbsolomon <bjsolomon@gmail.com>
2022-10-12 18:52:52 -0400

    using python

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 console.log("Hello World");
 console.log("this is an update")
-<<<<<<< HEAD
 console.log("the branches should not be synced")
-=======
 //blah
->>>>>>> e01bed2b765c13e37b60344f456d1ca323379570
+console.log("using python now")
